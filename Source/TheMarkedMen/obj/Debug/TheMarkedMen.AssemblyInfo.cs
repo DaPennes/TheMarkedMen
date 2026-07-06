e60490b3c82d6b448385d78b9049f96e0f0e24dd
DaPennes <armawarrior22@gmail.com>
2026-07-06 18:22:26 +0800

    Clean up SurvivorArmor textures, add wornGraphicClass
    - Delete 14 orphaned texture files (11 typo'd 'SuriviorArmor' + 3 legacy CamelCase)
    - Add explicit wornGraphicClass=Graphic_Multi to SurvivorArmor def (was relying on fragile auto-detection)

--- Source/TheMarkedMen/obj/Debug/TheMarkedMen.AssemblyInfo.cs
+++ Source/TheMarkedMen/obj/Debug/TheMarkedMen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheMarkedMen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efaf3c0df911a7ee982dd36b8154f62468f71836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4855deb7740cf99fdfecb9350017d43ed06ab221")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheMarkedMen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheMarkedMen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
